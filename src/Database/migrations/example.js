33f55c879e529ad4779fdfbc5c8310e82e096716
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-08-27 19:44:02 -0300

    migration updated

--- src/Database/migrations/example.js
+++ src/Database/migrations/example.js
@@ -1,6 +1,6 @@
 export const up = (knex) =>
-  knex.schema.createTable("table's name", (table) => {
+  knex.schema.createTable('put table name here', (table) => {
 
   });
 
-export const down = (knex) => knex.schema.dropTable("table's name");
+export const down = (knex) => knex.schema.dropTable('put table name here');
